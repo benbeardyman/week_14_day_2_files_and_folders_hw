a0e4ab40a3ebad54d1b176ab13a69dd66b877e82
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-03-07 16:38:51 +0000

    setup

---
+++ src/main/java/com/codeclan/example/filesFoldersService/FilesFoldersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filesFoldersService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesFoldersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesFoldersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/filesFoldersService/FilesFoldersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filesFoldersService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesFoldersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
